98f78d74731ced2aaed4ebed4225448f0fd5b2ae
yurykachatkou <yuri.kochetko@gmail.com>
2019-10-24 21:19:17 +0300

    Homework2: Yury Kachatkou

--- task4.py
+++ task4.py
@@ -8,5 +8,7 @@ for i in range(N):
     InStr = input().split()
     if InStr[1] == "1":
         res[i] = InStr[0]
-
-print(max(res, key=res.get) + 1)
+if len(res) == 0:
+    print(-1)
+else:
+    print(max(res, key=res.get) + 1)
