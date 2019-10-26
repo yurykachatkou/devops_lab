a09ede560466fe92aa553bfffb0bc3cd9aa3df51
yurykachatkou <yuri.kochetkov@gmail.com>
2019-10-26 09:54:28 -0400

    Homework3: Yury Kachatkou

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="snapshot",
+    scripts=[],
+    packages=find_packages(),
+    version="1.1",
+    author="Yury Kachatkou",
+    author_email="author@example.com",
+    description="Snapshot app",
+)
