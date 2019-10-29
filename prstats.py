81b18f51645d993ffe4ad755f312f8000fbdda15
yurykachatkou <yuri.kochetkov@gmail.com>
2019-10-29 17:50:21 -0400

    Homework4: Yury Kachatkou

--- prstats.py
+++ prstats.py
@@ -10,16 +10,16 @@ parser.add_argument('-r', '--repo', required=True,
                     help='Name of a repository')
 parser.add_argument('-u', '--user', required=False,
                     help='Info for user who made pull requests. '
-                    'If not specified, info for all users will be outputted')
+                         'If not specified, info for all users will be outputted')
 parser.add_argument('-op', '--option', required=False,
                     help='Input options: '
-                    '"state" - to get a state of PR, '
-                    '"title" - to get PR title, '
-                    '"id" - to get id of PR, '
-                    '"day" - to get a day of the week PR was opened, '
-                    '"weeks" - to get a number of week PR was opened, '
-                    '"title" - to get a title of PR. '
-                    'If not specified, all option will be outputted')
+                         '"state" - to get a state of PR, '
+                         '"title" - to get PR title, '
+                         '"id" - to get id of PR, '
+                         '"day" - to get a day of the week PR was opened, '
+                         '"weeks" - to get a number of week PR was opened, '
+                         '"title" - to get a title of PR. '
+                         'If not specified, all option will be outputted')
 parser.add_argument('-v', action="version", version="version 1.1")
 
 args = parser.parse_args()
@@ -40,11 +40,11 @@ r = requests.get(url, auth=(username, password))
 curdate = datetime.datetime.now()
 data = r.json()
 
-for i, v in enumerate(data):
-    if data[i]['user']['login'] == user or not user:
+for i in data:
+    if i['user']['login'] == user or not user:
         # get date of pull creation
-        crday = datetime.datetime.\
-            strptime(data[i]['created_at'][0:10], '%Y-%m-%d')
+        crday = datetime.datetime. \
+            strptime(i['created_at'][0:10], '%Y-%m-%d')
         # difference between current date and date of pull creation
         daydiff = str((crday - curdate).days)[1:]
         # dayofweek
@@ -52,24 +52,24 @@ for i, v in enumerate(data):
         # print day
         if option == 'day':
             print('Pull #{}. User:{}. Number of days: {}'.
-                  format(str(i + 1), data[i]['user']['login'], daydiff))
+                  format(i['number'], i['user']['login'], daydiff))
         # print week
         elif option == 'weeks':
             print('Pull #{}. User:{}. Day of the week opened: {}'.
-                  format(str(i + 1), data[i]['user']['login'], weeknum))
+                  format(i['number'], i['user']['login'], weeknum))
         # print every option
         elif not option:
             print('Pull #{}. User: {}. Number of days: {}. '
                   'Day of the week opened: {}. '
-                  'State: {}. Id: {}.'.
-                  format(str(i + 1),
-                         data[i]['user']['login'],
-                         daydiff, weeknum, data[i]['state'], data[i]['id']))
+                  'State: {}. Id: {}. Title: {}'.
+                  format(i['number'],
+                         i['user']['login'],
+                         daydiff, weeknum, i['state'], i['id'], i['title']))
         # print other options
         elif option == "state" or option == "id" or option == "title":
             print('Pull #{}. User: {}. {}: {}.'.
-                  format(str(i + 1),
-                         data[i]['user']['login'], option, data[i][option]))
+                  format(i['number'],
+                         i['user']['login'], option, i[option]))
         else:
             print('Wrong arguments. '
                   '"Type python prstats -h" To see help info.')
