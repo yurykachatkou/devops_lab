48b0142b3eca0ec57f1c17c66324a1bb1b7d21d4
yurykachatkou <yuri.kochetkov@gmail.com>
2019-10-29 18:53:02 -0400

    Homework5: Yury Kachatkou

--- sysoutput.py
+++ sysoutput.py
@@ -4,6 +4,7 @@ import os
 import pkg_resources
 import json
 import yaml
+import subprocess
 
 ver = sys.version[0:5]
 venvname = str(os.environ.get('VIRTUALENV')).split("/")[-1]
@@ -18,11 +19,16 @@ for i in pkg_resources.working_set:
         "Name: " + i.project_name + ",'Version: " + i.version)
 location = i.location
 
+verdata = subprocess.Popen(['pyenv', 'versions'], stdout=subprocess.PIPE)
+versions = verdata.stdout.read()
+versions = versions.decode('utf-8').replace(" ", "").split("\n")[:-1]
+
 file = {"Version": ver, "Virtual environment": venvname,
         "Executable location": sys.executable,
         "Pip Location": pipdir,
         "Python path": pythonpath, "Packages": packages,
-        "Site-packages location": location}
+        "Site-packages location": location,
+        "Python versions and environments": versions}
 
 with open("packages.json", 'w') as outfile:
     json.dump(file, outfile, indent=3)
